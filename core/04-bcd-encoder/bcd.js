9dc65c31a35c19a48c1a71f119ae45ccab452fad
DashaPoliazz <dasha.poliazzz@gmail.com>
2024-03-15 18:26:03 +0100

    methods 'get' & 'valueOf' has been refactored to bit operations

--- core/04-bcd-encoder/bcd.js
+++ core/04-bcd-encoder/bcd.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // [] Create BCD encoder
 // [] Add negative number support
 // [] Add ability to perform math operation
@@ -7,25 +9,30 @@
 //  []  - "/"
 
 class BCD {
+  #BIN_PREFIX = "0b";
+  #BCD_BIT_LEN = 4;
+
   #numbers;
 
   constructor(number) {
-    this.decimal = number;
+    this.#numbers = [];
+    this.number = number;
+    this.bcd = 0;
   }
 
   // decimal = 65536
   // returns 0b01100101010100110110 или 415030
   valueOf() {
-    // each n takes 8 bits, but each n in BCD - 4
-    const stringifiedDecimal = this.decimal.toString();
-    let bcd = "";
-    for (let i = stringifiedDecimal.length - 1; i >= 0; i--) {
-      const n = Number(stringifiedDecimal[i]);
-      const binaryRepresentation = n.toString(2).padStart(4, 0);
-      bcd = binaryRepresentation.concat(bcd);
+    let decimal = this.number;
+
+    for (let shift = 0; decimal > 0; shift += this.#BCD_BIT_LEN) {
+      const digit = decimal % 10;
+      const binDigit = digit.toString(2).padStart(4, 0);
+      this.bcd |= parseInt(binDigit, 2) << shift;
+      decimal = Math.floor(decimal / 10);
     }
-    bcd = "0b".concat(bcd);
-    return Number(bcd);
+
+    return this.bcd;
   }
 
   // decimal = 65536
@@ -35,14 +42,23 @@ class BCD {
   // idx= -1 =>6
   // idx= 02 => 5
   get(idx) {
-    const stringifiedDecimal = this.decimal.toString();
-    const littleEndianIndex = stringifiedDecimal.length - 1 - idx;
-    const negativeLittleEndianIndex = Math.abs(idx) - 1;
-    const normalizedIndex =
-      idx < 0 ? negativeLittleEndianIndex : littleEndianIndex;
-    console.log("IDX", normalizedIndex);
-    return stringifiedDecimal[normalizedIndex];
+    // It's possible to calc amount of nums in number with
+    // bit operation
+    const len = this.number.toString().length;
+
+    const shift = (idx < 0 ? len + idx : idx) * this.#BCD_BIT_LEN;
+    const readMask = 0b1111 << shift;
+    const result = (readMask & this.bcd) >> shift;
+    return result;
   }
+
+  add(summand) {}
+
+  substract(minuend) {}
+
+  multiply(multiplier) {}
+
+  divide(divisor) {}
 }
 
 const n = new BCD(65536);
@@ -53,3 +69,5 @@ console.log(n.get(1)); // 3
 
 console.log(n.get(-1)); // 6
 console.log(n.get(-2)); // 5
+console.log(n.get(-3)); // 5
+console.log(n.get(-4)); // 3
